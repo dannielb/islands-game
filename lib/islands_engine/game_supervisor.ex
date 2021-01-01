dfd89c3cf5405d6545499a9bbe3266221707a988
Daniel Bonfim <dannielbomfim4@gmail.com>
2021-01-01 13:10:45 -0300

    feat(game): implementing the main Game logic with a GameSupervisor

---
+++ lib/islands_engine/game_supervisor.ex
@@ -0,0 +1,26 @@
+defmodule IslandsEngine.GameSupervisor do
+  use DynamicSupervisor
+  alias IslandsEngine.Game
+
+  def start_link(_options), do: DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+
+  def init(:ok), do: DynamicSupervisor.init(strategy: :one_for_one)
+
+  def start_game(name) do
+    spec = %{id: name, start: {Game, :start_link, [name]}}
+    DynamicSupervisor.start_child(__MODULE__, spec)
+  end
+
+  def stop_game(name) do
+    case pid_from_name(name) do
+      pid when is_pid(pid) -> DynamicSupervisor.terminate_child(__MODULE__, pid)
+      nil -> {:error, :not_found}
+    end
+  end
+
+  defp pid_from_name(name) do
+    name
+    |> Game.via_tuple
+    |> GenServer.whereis
+  end
+end
